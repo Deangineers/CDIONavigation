200cd8187b5608e36b3c48dad25300a109f50128
emilsavino <s235096@dtu.dk>
2025-05-02 18:51:26 +0200

    Allocating commands to the stack instead of the heap for improved performance. Look Elias i learned something

--- src/MainController.cpp
+++ src/MainController.cpp
@@ -19,7 +19,7 @@ NavigationController* MainController::getNavController()
 
 void MainController::navigateAndSendCommand()
 {
-  auto command = std::make_unique<Command>();
+  Command command;
   std::pair<double, double> degreesAndDistanceToObject = navigationController_->calculateDegreesAndDistanceToObject();
   double degreesToTurn = degreesAndDistanceToObject.first;
   double distanceToObject = degreesAndDistanceToObject.second;
@@ -27,52 +27,52 @@ void MainController::navigateAndSendCommand()
   {
     if (degreesToTurn > 0)
     {
-      command->setAction("r");
+      command.setAction("r");
     }
     else
     {
-      command->setAction("l");
+      command.setAction("l");
     }
 
     if (degreesToTurn > - 20.0 && degreesToTurn < 20.0)
     {
-      command->setSpeed(100);
+      command.setSpeed(100);
     }
     else
     {
-      command->setSpeed(500);
+      command.setSpeed(500);
     }
-    client_->sendCommand(command->formatToSend());
+    client_->sendCommand(command.formatToSend());
     return;
   }
 
   if (distanceToObject > 0.0)
   {
-    command->setAction("f");
+    command.setAction("f");
 
     if (distanceToObject < 30.0)
     {
       updateBallCollectionAction("in");
-      command->setSpeed(100);
+      command.setSpeed(100);
     }
     else
     {
-      command->setSpeed(500);
+      command.setSpeed(500);
     }
-    client_->sendCommand(command->formatToSend());
+    client_->sendCommand(command.formatToSend());
     return;
   }
 
   updateBallCollectionAction("s");
-  command->setAction("s");
-  client_->sendCommand(command->formatToSend());
+  command.setAction("s");
+  client_->sendCommand(command.formatToSend());
   //TODO refine navigation, as we are just driving to the object and stopping at the moment
 }
 
 void MainController::updateBallCollectionAction(const std::string& action)
 {
-  const auto command = std::make_unique<Command>();
-  command->setAction(action);
-  command->setMotor("c");
-  client_->sendCommand(command->formatToSend());
+  Command command;
+  command.setAction(action);
+  command.setMotor("c");
+  client_->sendCommand(command.formatToSend());
 }
