19a5a560308e8d6689ba98445d6c606dca5a8bfc
eal <elias@takefive.dk>
2025-05-03 14:37:01 +0200

    added Gtest

---
+++ tests/NavigationControllerTests.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eal on 5/3/25.
+//
+
+#include "NavigationControllerTests.h"
---
+++ tests/NavigationControllerTests.h
@@ -0,0 +1,23 @@
+//
+// Created by eal on 5/3/25.
+//
+
+#ifndef NAVIGATIONCONTROLLERTESTS_H
+#define NAVIGATIONCONTROLLERTESTS_H
+#include <vector>
+#include "gtest/gtest.h"
+
+
+class NavigationControllerTests : public ::testing::Test
+{
+
+};
+
+
+TEST(PriceSorterTests, findSmallGroupTest1)
+{
+  EXPECT_TRUE(true);
+}
+
+
+#endif //NAVIGATIONCONTROLLERTESTS_H
