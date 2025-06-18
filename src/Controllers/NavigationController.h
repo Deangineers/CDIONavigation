b0181c8e2d82d92d909b5241143aacc759330024
Frederik Bastrup <frederik.bastrup@gmail.com>
2025-06-18 11:55:09 +0200

    fixed header

--- src/Controllers/NavigationController.h
+++ src/Controllers/NavigationController.h
@@ -43,7 +43,7 @@ private:
   [[nodiscard]] bool checkCollisionOnRoute(const Vector& targetVector) const;
   [[nodiscard]] bool frontIsToCloseToBlockingObject() const;
   Vector navigateToSafeSpot();
-  void NavigationController::findSafeSpots();
+  void findSafeSpots();
 
   bool targetStillActual();
 
