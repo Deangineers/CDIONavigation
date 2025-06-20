8d0158be8800627ce22f6a498c98712c1cf5f1e2
Elias Aggergaard Larsen <s235125@dtu.dk>
2025-06-20 18:33:56 +0200

    Removed unused masks that took a lot of time to compute

--- src/Controllers/ImageProcessing/CloudyImageProcessor.cpp
+++ src/Controllers/ImageProcessing/CloudyImageProcessor.cpp
@@ -32,28 +32,7 @@ void CloudyImageProcessor::detectRedPixels(const cv::Mat& frame)
 
 void CloudyImageProcessor::detectBalls(const cv::Mat& frame)
 {
-  cv::Mat totalMask;
-
-  cv::Mat darkestOrangeMask, darkOrangeMask, orangeMask, lightOrangeMask, lightestOrangeMask;
-  cv::Mat darkestWhiteMask, darkWhiteMask, whiteMask, lightWhiteMask, lightestWhiteMask;
-
-  cv::inRange(hsv_, cv::Scalar(5, 150, 30), cv::Scalar(10, 255, 130), darkestOrangeMask);
-  cv::inRange(hsv_, cv::Scalar(5, 150, 80), cv::Scalar(15, 255, 180), darkOrangeMask);
-  cv::inRange(hsv_, cv::Scalar(10, 100, 150), cv::Scalar(20, 255, 255), orangeMask);
-  cv::inRange(hsv_, cv::Scalar(10, 80, 180), cv::Scalar(25, 200, 255), lightOrangeMask);
-  cv::inRange(hsv_, cv::Scalar(10, 50, 200), cv::Scalar(30, 150, 255), lightestOrangeMask);
-
-  cv::inRange(hsv_, cv::Scalar(0, 0, 80), cv::Scalar(179, 40, 140), darkestWhiteMask);
-  cv::inRange(hsv_, cv::Scalar(0, 0, 150), cv::Scalar(179, 30, 180), darkWhiteMask);
-  cv::inRange(hsv_, cv::Scalar(0, 0, 180), cv::Scalar(179, 25, 220), whiteMask);
-  cv::inRange(hsv_, cv::Scalar(0, 0, 220), cv::Scalar(179, 20, 240), lightWhiteMask);
-  cv::inRange(hsv_, cv::Scalar(0, 0, 240), cv::Scalar(179, 20, 255), lightestWhiteMask);
-
-  cv::Mat orangeTotalMask = darkestOrangeMask | darkOrangeMask | orangeMask | lightOrangeMask | lightestOrangeMask;
-  cv::Mat whiteTotalMask = darkestWhiteMask | darkWhiteMask | whiteMask | lightWhiteMask | lightestWhiteMask;
-
-  //cv::bitwise_or(orangeTotalMask,whiteTotalMask, totalMask);
-  ballHelperFunction(frame, orangeTotalMask, "orange");
+  ballHelperFunction(frame, "orange");
 }
 
 void CloudyImageProcessor::detectEgg(const cv::Mat& frame)
@@ -73,7 +52,6 @@ void CloudyImageProcessor::detectFrontAndBack(const cv::Mat& frame)
 
   cv::inRange(hsv_, cv::Scalar(35, 50, 50), cv::Scalar(85, 255, 255), frontMask);
   cv::inRange(hsv_, cv::Scalar(120, 85, 85), cv::Scalar(170, 255, 255), backMask);
-
   frontAndBackHelperFunction(frame, frontMask, "robotFront");
   frontAndBackHelperFunction(frame, backMask, "robotBack");
 }
