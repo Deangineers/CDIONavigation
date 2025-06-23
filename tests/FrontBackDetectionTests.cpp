a0dd1356d50296b11675981b158c683074b68242
EAL <s235125@dtu.dk>
2025-06-23 08:39:54 +0200

    fixed error

--- tests/FrontBackDetectionTests.cpp
+++ tests/FrontBackDetectionTests.cpp
@@ -31,7 +31,7 @@ public:
 
 TEST_F(FrontBackDetectionTests, testImages)
 {
-  int amountOfImages = 25;
+  int amountOfImages = 24;
   for (int i = 1; i <= amountOfImages; i++)
   {
     cv::Mat frame = cv::imread("../../TestImages/testImage"+std::to_string(i)+".jpg");
